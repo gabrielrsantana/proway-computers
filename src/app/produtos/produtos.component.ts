8cf4675c6b33781230bdc90aa8867c99d7b6d01d
Gabriel Rocha <58997038+gabrielrsantana@users.noreply.github.com>
2022-07-09 14:20:05 -0300

    assets links para produtos

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { produtos, IProduto } from '../produtos';
+
+
 
 @Component({
   selector: 'app-produtos',
@@ -6,6 +9,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produtos.component.css']
 })
 export class ProdutosComponent implements OnInit {
+//produtos é o vetor dos produtos
+//iProduto é a interface
+produtos: IProduto[] = produtos;
 
   constructor() { }
 
