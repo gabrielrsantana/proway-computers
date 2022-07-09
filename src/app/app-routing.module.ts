e2c83aed6ccf42248974c759f1ba1a190ab2959b
Gabriel Rocha <58997038+gabrielrsantana@users.noreply.github.com>
2022-07-09 13:37:55 -0300

    template produtos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 //import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes =[{ path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) }];
+//usando lazy Loading para os produtos
+const routes: Routes =[
+  { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
+  { path: "", redirectTo: "produtos", pathMatch: "full"} //quando colocar raiz so site ele direciona pro /produtos
+];
 
 @NgModule({
   declarations: [],
