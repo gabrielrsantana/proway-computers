680d4459b7dd6fdf59c0e2a7a94c92de008da98c
Gabriel Rocha <58997038+gabrielrsantana@users.noreply.github.com>
2022-07-09 14:33:11 -0300

    pagina nao encontrada component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 //import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { NaoEncontradaComponent } from './nao-encontrada/nao-encontrada.component';
 
 //usando lazy Loading para os produtos
 const routes: Routes =[
   { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
-  { path: "", redirectTo: "produtos", pathMatch: "full"} //quando colocar raiz so site ele direciona pro /produtos
+  { path: "", redirectTo: "produtos", pathMatch: "full"}, //quando colocar raiz so site ele direciona pro /produtos
+  { path:"**", component: NaoEncontradaComponent} 
 ];
 
 @NgModule({
